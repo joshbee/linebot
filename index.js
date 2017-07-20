72ab78499b5e9a7b6db29cd6705b333c665d869d
DESKTOP-UFM1S96\josh <josh@ashlieworks.com>
2017-07-20 18:02:12 +0800

    no message

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ bot.on('message', function(event) {
 		}else if (msg.indexOf('我是誰') != -1) {
 			bot.getUserProfile(event.source.userId).then(function (data) {
 				event.reply('你是' + data);
+				console.log(data);
 			}).catch(function (error) {
 				// error 
 			});
